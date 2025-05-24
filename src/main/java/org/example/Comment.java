bbc1925ed2d993f5691c4c089368471724949e3c
Iliya Raev <insomniq4@abv.bg>
2025-05-24 16:36:23 +0300

    Create class with fields and constructors

--- src/main/java/org/example/Comment.java
+++ src/main/java/org/example/Comment.java
@@ -1,4 +1,31 @@
 package org.example;
 
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Entity
 public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String text;
+    private LocalDateTime createdAt;
+
+    @ManyToOne
+    @JoinColumn(name = "task_id")
+    private Task task;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    public Comment() {
+
+    }
+
+    Comment(String text, LocalDateTime createdAt) {
+        this.text = text;
+        this.createdAt = createdAt;
+    }
 }
