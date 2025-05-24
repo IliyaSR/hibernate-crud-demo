8cd5b0a4ed9e2f36104fbf6bd86b187d4ea2683f
Iliya Raev <insomniq4@abv.bg>
2025-05-24 17:56:59 +0300

    Create UserDAO class with createUser method

--- src/main/java/org/example/UserDAO.java
+++ src/main/java/org/example/UserDAO.java
@@ -1,4 +1,29 @@
 package org.example;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+
 public class UserDAO {
+
+    private SessionFactory sessionFactory;
+
+    public UserDAO(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+
+    }
+
+    public void createUser(User user) {
+        Transaction tx = null;
+
+        try(Session session = sessionFactory.openSession()){
+            tx = session.beginTransaction();
+            session.persist(user);
+            tx.commit();
+        }catch(Exception e){
+            if(tx != null) tx.rollback();
+            throw e;
+        }
+    }
+
 }
