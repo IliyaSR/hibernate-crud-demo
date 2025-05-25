7fc3b1028983decfcb2ffd8e9e36b2ae531b352f
Iliya Raev <insomniq4@abv.bg>
2025-05-25 16:28:38 +0300

    Create CRUD operations

--- src/main/java/org/example/dao/UserDAO.java
+++ src/main/java/org/example/dao/UserDAO.java
@@ -1,4 +1,4 @@
-package org.example;
+package org.example.dao;
 
 import org.example.model.User;
 import org.hibernate.Session;
@@ -28,11 +28,49 @@ public class UserDAO {
     }
 
     public User getUser(int id) {
-        Session session = sessionFactory.openSession();
-        User user = session.get(User.class, id);
+        try (Session session = sessionFactory.openSession()) {
+            return session.get(User.class, id);
+        }
+    }
 
-        return user;
+    public void updateUser(int id, String username, String email) {
+        Transaction tx = null;
 
+        try (Session session = sessionFactory.openSession()) {
+            tx = session.beginTransaction();
+
+            User user = session.get(User.class, id);
+            if (username != null) {
+                user.setUsername(username);
+            }
+            if (email != null) {
+                user.setEmail(email);
+            }
+
+            session.merge(user);
+
+            tx.commit();
+        } catch (Exception e) {
+            if (tx != null) {
+                tx.rollback();
+            }
+            e.printStackTrace();
+        }
+    }
+
+    public void deleteUser(int id) {
+        Transaction tx = null;
+
+        try (Session session = sessionFactory.openSession()) {
+            tx = session.beginTransaction();
+            session.remove(session.get(User.class, id));
+            tx.commit();
+        }catch(Exception e){
+            if(tx != null){
+                tx.rollback();
+            }
+            e.printStackTrace();
+        }
     }
 
 }
