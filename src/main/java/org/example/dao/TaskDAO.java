ccf2f5ae0a43fc972f0a92dd765d2a7d0cacd64b
Iliya Raev <insomniq4@abv.bg>
2025-05-25 16:28:05 +0300

    Create CRUD operations

--- src/main/java/org/example/dao/TaskDAO.java
+++ src/main/java/org/example/dao/TaskDAO.java
@@ -1,14 +1,15 @@
-package org.example;
+package org.example.dao;
 
 import org.example.model.Task;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
+import org.jetbrains.annotations.Nullable;
 
 public class TaskDAO {
     private SessionFactory sessionFactory;
 
-    TaskDAO(SessionFactory sessionFactory) {
+    public TaskDAO(SessionFactory sessionFactory) {
         this.sessionFactory = sessionFactory;
     }
 
@@ -22,6 +23,57 @@ public class TaskDAO {
         } catch (Exception e) {
             if (tx != null) tx.rollback();
             throw e;
+        }
+    }
+
+    public Task getTask(int id) {
+        try (Session session = sessionFactory.openSession()) {
+            return session.get(Task.class, id);
+        }
+    }
+
+    public void deleteTask(int id) {
+        Transaction tx = null;
+
+        try (Session session = sessionFactory.openSession()) {
+            tx = session.beginTransaction();
+
+            session.remove(session.get(Task.class, id));
+
+            tx.commit();
+        } catch (Exception e) {
+            if (tx != null) {
+                tx.rollback();
+            }
+            e.printStackTrace();
+        }
+    }
+
+    public void updateTask(int id, String title, String description, String status) {
+        Transaction tx = null;
+
+        try (Session session = sessionFactory.openSession()) {
+            tx = session.beginTransaction();
+
+            Task task = session.get(Task.class, id);
+
+            if (title != null) {
+                task.setTitle(title);
+            }
+            if (description != null) {
+                task.setDescription(description);
+            }
+            if (status != null) {
+                task.setStatus(status);
+            }
+
+            session.merge(task);
+            tx.commit();
+        } catch (Exception e) {
+            if (tx != null) {
+                tx.rollback();
+            }
+            e.printStackTrace();
         }
     }
 }
