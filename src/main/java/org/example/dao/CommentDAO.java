d53372bef7ae8fd66c48d8b5da857558d7bab314
Iliya Raev <insomniq4@abv.bg>
2025-05-25 16:27:12 +0300

    Create CommentDAO class with CRUD operations

--- src/main/java/org/example/dao/CommentDAO.java
+++ src/main/java/org/example/dao/CommentDAO.java
@@ -1,4 +1,75 @@
 package org.example.dao;
 
+import org.example.model.Comment;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+
 public class CommentDAO {
+    private SessionFactory sessionFactory;
+
+    public CommentDAO(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+    }
+
+    public void createComment(Comment comment) {
+        Transaction tx = null;
+
+        try (Session session = sessionFactory.openSession()) {
+            tx = session.beginTransaction();
+
+            session.persist(comment);
+
+            tx.commit();
+        } catch (Exception e) {
+            if (tx != null) {
+                tx.rollback();
+            }
+            e.printStackTrace();
+        }
+    }
+
+    public Comment getComment(int id) {
+        try (Session session = sessionFactory.openSession()) {
+            return session.get(Comment.class, id);
+        }
+    }
+
+    public void updateComment(int id, String text) {
+        Transaction tx = null;
+
+        try (Session session = sessionFactory.openSession()) {
+            tx = session.beginTransaction();
+
+            Comment comment = session.get(Comment.class, id);
+
+            if (text != null) {
+                comment.setText(text);
+            }
+
+            session.merge(comment);
+            tx.commit();
+        } catch (Exception e) {
+            if (tx != null) {
+                tx.rollback();
+            }
+            e.printStackTrace();
+        }
+    }
+
+    public void deleteComment(int id) {
+        Transaction tx = null;
+
+        try (Session session = sessionFactory.openSession()) {
+            tx = session.beginTransaction();
+
+            session.remove(session.get(Comment.class, id));
+            tx.commit();
+        } catch (Exception e) {
+            if (tx != null) {
+                tx.rollback();
+            }
+            e.printStackTrace();
+        }
+    }
 }
